0439ecd10d70496b27b45eafed2638629dbd199e
Mikkel Ricky <rimi@aarhus.dk>
2022-06-27 15:05:13 +0200

    HRIN-707: Added SOAP call for adding document to case

--- src/Service.php
+++ src/Service.php
@@ -19,13 +19,23 @@ abstract class Service
     }
 
     /**
+     * Send request.
+     *
+     * @throws GetOrganizedClientException
+     */
+    protected function request(string $method, string $url, array $options = [])
+    {
+        return $this->client->request($method, $url, $options);
+    }
+
+    /**
      * Get data from API endpoint.
      *
      * @throws GetOrganizedClientException
      */
     protected function getData(string $method, string $url, array $options = [])
     {
-        $response = $this->client->request($method, $url, $options);
+        $response = $this->request($method, $url, $options);
 
         // The response body may be empty which will throw an exception in
         // Response::toArray(), but we don't want any errors in that case.
